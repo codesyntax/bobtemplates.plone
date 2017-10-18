ebdc943e13a8d055a32b088affbf83bb655ee62c
MrTango <md@derico.de>
2017-10-19 00:56:17 +0200

    add subtemplates entrypoints

--- bobregistry.py
+++
@@ -1,7 +0,0 @@
-
-def get_plone_addon():
-    return 'bobtemplates.plone:addon'
-
-
-def get_plone_buildout():
-    return 'bobtemplates.plone:buildout'
---
+++ bobtemplates/plone/bobregistry.py
@@ -0,0 +1,23 @@
+
+def get_addon():
+    return 'bobtemplates.plone:addon'
+
+
+def get_buildout():
+    return 'bobtemplates.plone:buildout'
+
+
+def get_theme_package():
+    return 'bobtemplates.plone:theme_package'
+
+
+def get_theme():
+    return 'bobtemplates.plone:theme'
+
+
+def get_content_type():
+    return 'bobtemplates.plone:content_type'
+
+
+def get_vocabulary():
+    return 'bobtemplates.plone:vocabulary'
