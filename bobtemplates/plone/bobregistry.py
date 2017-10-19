9e2c952b57487192f59c1a9a3b27aa87a0011c87
MrTango <md@derico.de>
2017-10-19 16:24:04 +0200

    use more rich entry points and nesting

--- bobtemplates/plone/bobregistry.py
+++ bobtemplates/plone/bobregistry.py
@@ -1,23 +1,48 @@
-
-def get_addon():
-    return 'bobtemplates.plone:addon'
-
-
-def get_buildout():
-    return 'bobtemplates.plone:buildout'
+class RegEntry(object):
+    def __init__(self):
+        self.template = ''
+        self.type = ''
+        self.depend_on = None
 
 
-def get_theme_package():
-    return 'bobtemplates.plone:theme_package'
+def plone_addon():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:addon'
+    reg.type = 'template'
 
 
-def get_theme():
-    return 'bobtemplates.plone:theme'
+def plone_buildout():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:buildout'
+    reg.type = 'template'
 
 
-def get_content_type():
-    return 'bobtemplates.plone:content_type'
+def plone_theme_package():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:theme_package'
+    reg.type = 'template'
+    return reg
 
 
-def get_vocabulary():
-    return 'bobtemplates.plone:vocabulary'
+def theme():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:theme'
+    reg.type = 'subtemplate'
+    reg.depend_on = 'plone_addon'
+    return reg
+
+
+def content_type():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:content_type'
+    reg.type = 'subtemplate'
+    reg.depend_on = 'plone_addon'
+    return reg
+
+
+def vocabulary():
+    reg = RegEntry()
+    reg.template = 'bobtemplates.plone:vocabulary'
+    reg.type = 'subtemplate'
+    reg.depend_on = 'plone_addon'
+    return reg
