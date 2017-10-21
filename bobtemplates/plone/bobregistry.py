458211842bea7789bab7a95521a12cadb1c0087d
MrTango <md@derico.de>
2017-10-21 12:07:36 +0200

    fix bobregistry and reorder entrypoints

--- bobtemplates/plone/bobregistry.py
+++ bobtemplates/plone/bobregistry.py
@@ -1,33 +1,30 @@
 class RegEntry(object):
     def __init__(self):
         self.template = ''
-        self.type = ''
         self.depend_on = None
 
 
 def plone_addon():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:addon'
-    reg.type = 'template'
+    return reg
 
 
 def plone_buildout():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:buildout'
-    reg.type = 'template'
+    return reg
 
 
 def plone_theme_package():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:theme_package'
-    reg.type = 'template'
     return reg
 
 
 def theme():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:theme'
-    reg.type = 'subtemplate'
     reg.depend_on = 'plone_addon'
     return reg
 
@@ -35,7 +32,6 @@ def theme():
 def content_type():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:content_type'
-    reg.type = 'subtemplate'
     reg.depend_on = 'plone_addon'
     return reg
 
@@ -43,6 +39,5 @@ def content_type():
 def vocabulary():
     reg = RegEntry()
     reg.template = 'bobtemplates.plone:vocabulary'
-    reg.type = 'subtemplate'
     reg.depend_on = 'plone_addon'
     return reg
